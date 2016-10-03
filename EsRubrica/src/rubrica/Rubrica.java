004a9b7cf6376c7ad2e307524a4954aa4e7c33a5
Pierangelo <pierangelo92@live.it>
2016-10-03 16:20:36 +0200

    int linea 24

--- EsRubrica/src/rubrica/Rubrica.java
+++ EsRubrica/src/rubrica/Rubrica.java
@@ -21,7 +21,7 @@ public class Rubrica {
 	}
 	
 	
-	int b;
+	int bfdnkn;
 	public Voce addVoce(String nome, String cognome, String telefono) throws VoceEsistente {
 		Voce voce = null;
 		if (listaVoci.containsKey(nome+" "+cognome)){
