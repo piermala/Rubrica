f84230c5112010a1d767bd4c6707e2e7afb686ea
Pierangelo <pierangelo92@live.it>
2016-10-04 12:11:03 +0200

    Creazione metodi aggiorna e cancella

--- EsRubrica/src/main/MainRubrica.java
+++ EsRubrica/src/main/MainRubrica.java
@@ -6,8 +6,20 @@ public class MainRubrica {
 	public static void main(String[] args) throws VoceEsistente {
 		
 		Rubrica r = new Rubrica();
+		
 		Voce v1 = r.addVoce("Mario", "Rossi", "3334455667");
+		/*Voce v1 = r.addVoce("Mario", "Rossi", "3334455667");
+		Voce v1 = r.addVoce("Mario", "Rossi", "3334455667");
+		Voce v1 = r.addVoce("Mario", "Rossi", "3334455667");*/
 		System.out.println(v1);
+		
+		/// AGGIORNA VOCE
+		System.out.println(r.aggiornaVoce("Mario", "Rossi", "3316429670"));
+		
+		/// CANCELLA VOCE
+		r.cancellaVoce("Mario", "Rossi");
+		
+		System.out.println(r.getListaVoci().size());
 	}
 }
 
--- EsRubrica/src/rubrica/Rubrica.java
+++ EsRubrica/src/rubrica/Rubrica.java
@@ -41,14 +41,33 @@ public class Rubrica {
 	}
 	
 	
-	public Voce aggiornaVoce(String nome, String cognome, String telefono){
+	/// AGGIORNA VOCE
+	public Voce aggiornaVoce(String nome, String cognome, String telefono) throws VoceEsistente{
 		Voce v = null;
-		for (Voce v : listaVoci.get(nome+" "+cognome));
+		for (Map.Entry<String, Voce> map : listaVoci.entrySet()){
+			if (map.getKey().equals(nome+" "+cognome)){
+				v = new Voce(nome, cognome, telefono);
+				listaVoci.put(nome+" "+cognome, v);
+			} else {
+				throw new VoceEsistente("L'utente non esiste!");
+			}
+		}		
+		return v;
 	}
 	
 	
-	public Voce cancellaVoce(String nome, String cognome){
-		
+	/// CANCELLA VOCE
+	public Voce cancellaVoce(String nome, String cognome) throws VoceEsistente{
+		Voce v = null;
+		for (Map.Entry<String, Voce> map : listaVoci.entrySet()){
+			if (map.getKey().equals(nome+" "+cognome)){
+				v = new Voce(nome, cognome, listaVoci.get(nome+" "+cognome).getTelefono());
+				listaVoci.remove(nome+" "+cognome);
+			} else {
+				throw new VoceEsistente("L'utente non esiste!");
+			}
+		}		
+		return v;
 	}
 	
 	
